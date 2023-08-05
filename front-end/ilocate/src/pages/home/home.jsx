65d2eb5b566c754315de861c9cec8c7354b71e15
majed <majedhabli2020@gmail.com>
2023-08-05 18:43:20 +0300

    Updated the home page styles.

--- front-end/ilocate/src/pages/home/home.jsx
+++ front-end/ilocate/src/pages/home/home.jsx
@@ -10,6 +10,13 @@ const Home = () =>{
             <div className={styles.container_right}>
                 <div className={styles.container_description}>
                     <h1>Interested in someone?<br/> <span>Let's find them !</span></h1>
+                    <div>
+                        <ul>
+                            <li className={styles.description_list}>Add</li>
+                            <li className={styles.description_list}>Remove</li>
+                            <li className={styles.description_list}>and look up the positions of people you hold dear.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
