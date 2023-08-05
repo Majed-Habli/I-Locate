bd68ee913f41a937e432fd38b25cec18138eabd2
majed <majedhabli2020@gmail.com>
2023-08-05 21:19:26 +0300

    Updated the login page.

--- front-end/ilocate/src/pages/login/login.jsx
+++ front-end/ilocate/src/pages/login/login.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./login.module.css";
 import { Footer } from "../../components/footerComponent/footer";
 
 const Login = () =>{
+    const [email,setEmail] = useState('');
+    const [password,setPassword]=useState('');
+
     return(
         <div className={styles.container}>
             <div className={styles.container_left}>
                 <img src="/hero-map.png" alt="" />
             </div>
             <div className={styles.container_right}>
-                
+                <div className={styles.input_container}>
+                    <div className={styles.row}>
+                        <label className="label">Email: </label>
+                        <input value={email} onChange={ e=> setEmail(e.target.value) }/>
+                    </div>
+                    <div className={styles.row}>
+                        <label className="label">Password: </label>
+                        <input value={password} onChange={ e=> setPassword(e.target.value) }/>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
