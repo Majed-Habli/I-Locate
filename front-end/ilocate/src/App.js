260a5068c17471824106790ec420cdb82ff02a00
majed <majedhabli2020@gmail.com>
2023-08-06 18:04:18 +0300

    Updated the app.js.

--- front-end/ilocate/src/App.js
+++ front-end/ilocate/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/navbarComponent/navbar';
-import Home from './pages/home/home';
-import Login from './pages/login/login'
-import Register from './pages/register/register';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
       </Routes>
     </Router>
   );
